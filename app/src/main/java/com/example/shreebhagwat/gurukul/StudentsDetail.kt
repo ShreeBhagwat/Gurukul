26201dba54c3e9ee786d7704a6e4bb7bfa10b555
Shree Bhagwat <shreebhagwat94@gmail.com>
2018-08-05 17:32:54 +0530

    Add Student Page Created

--- app/src/main/java/com/example/shreebhagwat/gurukul/StudentsDetail.kt
+++ app/src/main/java/com/example/shreebhagwat/gurukul/StudentsDetail.kt
@@ -1,7 +1,15 @@
 package com.example.shreebhagwat.gurukul
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_students_detail.*
 
 class StudentsDetail : AppCompatActivity() {
 
@@ -10,5 +18,38 @@ class StudentsDetail : AppCompatActivity() {
         setContentView(R.layout.activity_students_detail)
 
         supportActionBar?.title = "Add Students"
+
+        st_submit.setOnClickListener {
+            saveStudentsDetailsToFirebase()
+        }
+
+        st_photo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+        }
+    }
+
+    var selectedPhoto: Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null ){
+            // proceed and check what the selector image was
+            Log.d("RegisterActivity","Photo was selected")
+
+            selectedPhoto = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhoto)
+
+//            select_photoImgaeView_register.setImageBitmap(bitmap)
+//            selectphoto_button_register.alpha = 0f
+
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            st_photo.setBackgroundDrawable(bitmapDrawable)
+            st_photo.text = null
+        }
+    }
+    private fun saveStudentsDetailsToFirebase(){
+
     }
 }
