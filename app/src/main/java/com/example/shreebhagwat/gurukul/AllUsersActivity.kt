219e959a3935b190551ce8fe3f6e8c58081d5b9a
Shree Bhagwat <shreebhagwat94@gmail.com>
2018-08-05 21:46:52 +0530

    Done Fetching student data on All Student view

--- app/src/main/java/com/example/shreebhagwat/gurukul/AllUsersActivity.kt
+++ app/src/main/java/com/example/shreebhagwat/gurukul/AllUsersActivity.kt
@@ -3,9 +3,20 @@ package com.example.shreebhagwat.gurukul
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_all_users.*
+import kotlinx.android.synthetic.main.all_users_layout.view.*
 
 class AllUsersActivity : AppCompatActivity() {
 
@@ -14,11 +25,46 @@ class AllUsersActivity : AppCompatActivity() {
         setContentView(R.layout.activity_all_users)
 
         verifyUsedLoggedIn()
+
+//        val adapter = GroupAdapter<ViewHolder>()
+//        adapter.add(StudentsItem())
+//        adapter.add(StudentsItem())
+//        adapter.add(StudentsItem())
+//        recycleview_alllusers.adapter = adapter
+
+        fetchStudents()
     }
 
-    private fun verifyUsedLoggedIn(){
+    private fun fetchStudents(){
+       val stuRef = FirebaseDatabase.getInstance().getReference("users/students")
+            stuRef.addListenerForSingleValueEvent(object : ValueEventListener{
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    val adapter = GroupAdapter<ViewHolder>()
+
+                    p0.children.forEach{
+                        it.toString()
+                        Log.d("AllUsersActivity", it.toString())
+                        val students = it.getValue(Students::class.java)
+                        if (students != null){
+                            adapter.add(StudentsItem(students))
+                        }
+
+                    }
+                    recycleview_alllusers.adapter = adapter
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+            })
+
+    }
+
+
+    private fun verifyUsedLoggedIn() {
         val uid = FirebaseAuth.getInstance().uid
-        if (uid == null){
+        if (uid == null) {
             val intent = Intent(this, RegisterActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
@@ -47,8 +93,25 @@ class AllUsersActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu,menu)
+        menuInflater.inflate(R.menu.nav_menu, menu)
         return super.onCreateOptionsMenu(menu)
 
     }
+
 }
+
+class StudentsItem(val student: Students): Item<ViewHolder>(){
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        // Will be called in list for each of the student object
+        viewHolder.itemView.studentName_textview_Allusers.text = student.studentsName
+
+        Picasso.get().load(student.studentImageUrl).into(viewHolder.itemView.imageview_AllUsers)
+    }
+    override fun getLayout(): Int {
+        return R.layout.all_users_layout
+    }
+
+
+}
+
